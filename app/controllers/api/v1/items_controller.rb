e58b397bb6fbfc969ece8bd0be29533c3988f665
Charlotte Moore <mooc123@gmail.com>
2017-04-13 09:15:06 -0600

    added explicit status code to api output

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::ItemsController < ApplicationController
 
   def index
-    render json: Item.all
+    render status: 200, json: Item.all
   end
 
   def show
-    render json: Item.find(params[:id])
+    render status: 200, json: Item.find(params[:id])
   end
 
 end
