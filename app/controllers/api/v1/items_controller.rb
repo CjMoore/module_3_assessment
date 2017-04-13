c3048d918dded4bc81c5a0d7a3f8d7be31098013
Charlotte Moore <mooc123@gmail.com>
2017-04-13 11:31:53 -0600

    made stuff more secure by changing protect_from_forgery workaround

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ItemsController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  skip_before_filter :verigy_authenticity_token, if: :json_request?
 
   def index
     render status: 200, json: Item.all, only: [:name, :description, :image_url]
