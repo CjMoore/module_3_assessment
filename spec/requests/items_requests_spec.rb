9770b81931afc2f7194b531a4555ec75adfb863c
Charlotte Moore <mooc123@gmail.com>
2017-04-13 09:13:33 -0600

    get show for items api route and test added and passing

--- spec/requests/items_requests_spec.rb
+++ spec/requests/items_requests_spec.rb
@@ -18,3 +18,20 @@ describe "get index" do
     expect(item["description"]).to eq("its a thing")
   end
 end
+
+describe "get show" do
+  it "when user visits /api/v1/items/:id they are returned the data for that item" do
+    item1 = Item.create(name: "item1", description: "its a thing")
+    item2 = Item.create(name: "item2", description: "its another thing")
+
+    get "/api/v1/items/#{item1.id}"
+
+    expect(response).to be_success
+
+    item = JSON.parse(response.body)
+
+    expect(item["name"]).to eq(item1.name)
+    expect(item["description"]).to eq(item1.description)
+    expect(item["name"]).to_not eq(item2.name)
+  end
+end
